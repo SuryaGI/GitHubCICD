3191f6ed872619ec0bb1967ace6d46d247eaab48
Suryakanta Samal <107230988+SuryaGI@users.noreply.github.com>
2023-01-04 23:05:24 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "hi sahu how are you?!";
 	}
 
 //	echo "# github-actions-example" >> README.md
